f9ac9a5ab63d6feb66b83e4abb0da222bd74c95f
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-07 09:11:27 -0600

    Created Request.parseJWT extension method

---
+++ Sources/Helpers/Request+JWT.swift
@@ -0,0 +1,17 @@
+import HTTP
+import JWT
+import AuthProvider
+
+extension Request {
+    func parseJWT() throws -> JWT {
+        guard let authHeader = auth.header else {
+            throw AuthenticationError.noAuthorizationHeader
+        }
+        
+        guard let bearer = authHeader.bearer else {
+            throw AuthenticationError.invalidBearerAuthorization
+        }
+        
+        return try JWT(token: bearer.string)
+    }
+}
