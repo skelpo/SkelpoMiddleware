84f5fe43d34259cd6405acb11dc50b8d9c444a32
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-07 09:14:51 -0600

    Added AWTAuthenticationMiddleware from outside project

---
+++ Sources/AuthMiddleware/JWTAuthenticationMiddlware.swift
@@ -0,0 +1,51 @@
+import Vapor
+import HTTP
+import JWTProvider
+import JWT
+import AuthProvider
+import Helpers
+
+public final class JWTAuthenticationMiddleware: Middleware {
+    private(set) var signers: SignerMap
+    let url: String
+    let claims: [Claim]
+    let client: ClientFactoryProtocol
+    
+    public init(url: String, claims: [Claim] = []) {
+        self.url = url
+        self.claims = claims
+        
+        self.signers = SignerMap()
+        self.client = EngineClientFactory()
+    }
+    
+    public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
+        let jwt = try request.parseJWT()
+        let signer = try self.signer(for: jwt)
+        _ = try request.jwt(verifyUsing: signer, and: claims)
+        
+        return try next.respond(to: request)
+    }
+    
+    private func signer(for jwt: JWT) throws -> Signer {
+        guard let kid = jwt.keyIdentifier else {
+            throw JWTProviderError.noVerifiedJWT
+        }
+        
+        if let signer = self.signers[kid] {
+            return signer
+        }
+        
+        guard let jwks = try self.client.get(url).json else {
+            throw JWTProviderError.noJWTSigner
+        }
+        
+        self.signers = try SignerMap(jwks: jwks)
+        
+        guard let signer = self.signers[kid] else {
+            throw JWTProviderError.noJWTSigner
+        }
+        
+        return signer
+    }
+}
