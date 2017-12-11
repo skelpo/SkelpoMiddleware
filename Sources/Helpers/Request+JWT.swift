e2c1609b59614ef58a3ffe221a9992d6206770de
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-11 11:33:36 -0600

    Changed Request.payload property to a method and created Request.teams method

--- Sources/Helpers/Request+JWT.swift
+++ Sources/Helpers/Request+JWT.swift
@@ -3,6 +3,9 @@ import HTTP
 import JWT
 import AuthProvider
 import JWTProvider
+import AuthMiddleware
+import APIMiddleware
+import Errors
 
 extension Request {
     public func parseJWT() throws -> JWT {
@@ -17,7 +20,17 @@ extension Request {
         return try JWT(token: bearer.string)
     }
     
-    public var payload: JSON {
-        return jwtPayload!
+    public func payload()throws -> JSON {
+        guard let payload = jwtPayload else {
+            throw MiddlewareError.middlewareNotRegistered(JWTAuthenticationMiddleware.self)
+        }
+        return payload
+    }
+    
+    public func teams()throws -> [Int] {
+        guard let teams = teamIDs else {
+            throw MiddlewareError.middlewareNotRegistered(TeamIDMiddleware.self)
+        }
+        return teams
     }
 }
