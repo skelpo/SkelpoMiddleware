2db5cf4383268d5d05e8de351c80ad27f4893d6f
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-11 11:33:01 -0600

    Created TeamIDMiddleware middleware class

---
+++ Sources/APIMiddleware/TeamIDMiddleware.swift
@@ -0,0 +1,12 @@
+import Vapor
+import Helpers
+import Store
+
+public final class TeamIDMiddleware: Middleware {
+    public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
+        let payload = try request.payload()
+        let teams: [Int] = try payload.get("team_ids")
+        teamIDs = teams
+        return try next.respond(to: request)
+    }
+}
