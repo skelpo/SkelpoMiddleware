7f09d171eac99191f9ebe262efce4f8a08924d85
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-12 07:40:25 -0600

    Made TeamIDMiddleware.init public

--- Sources/APIMiddleware/TeamIDMiddleware.swift
+++ Sources/APIMiddleware/TeamIDMiddleware.swift
@@ -2,6 +2,9 @@ import Vapor
 import Helpers
 
 public final class TeamIDMiddleware: Middleware {
+    
+    public init() {}
+    
     public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
         let payload = try request.payload()
         let teams: [Int] = try payload.get("team_ids")
