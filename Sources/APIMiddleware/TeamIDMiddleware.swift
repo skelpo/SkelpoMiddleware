694e4bbc227d59293b67b2fdafdd6d533532435a
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-12 10:15:46 -0600

    Made teams array in TeamIDMiddleware.respond optional

--- Sources/APIMiddleware/TeamIDMiddleware.swift
+++ Sources/APIMiddleware/TeamIDMiddleware.swift
@@ -7,7 +7,7 @@ public final class TeamIDMiddleware: Middleware {
     
     public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
         let payload = try request.payload()
-        let teams: [Int] = try payload.get("team_ids")
+        let teams: [Int]? = try payload.get("team_ids")
         request.storage["skelpo_teams"] = teams
         
         return try next.respond(to: request)
