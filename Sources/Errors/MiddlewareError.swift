622352828c776dbf900ed3cea652e2c08ce7c01b
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-15 11:45:43 -0600

    Conformed MiddlewareError. AbortError protocol

--- Sources/Errors/MiddlewareError.swift
+++ Sources/Errors/MiddlewareError.swift
@@ -1,5 +1,18 @@
 import Vapor
+import HTTP
 
-public enum MiddlewareError: Error {
+public enum MiddlewareError: AbortError {
     case middlewareNotRegistered(String)
+    
+    public var status: Status {
+        switch self {
+        case .middlewareNotRegistered: return .internalServerError
+        }
+    }
+    
+    public var reason: String {
+        switch self {
+        case let .middlewareNotRegistered(middleware): return "Make sure you register \(middleware) with your routes"
+        }
+    }
 }
