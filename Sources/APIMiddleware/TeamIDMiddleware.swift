d97bb9a05fd32b0b7816b76e7b21e84994d266d5
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-30 12:25:14 -0600

    Updated TeamIDMiddleware for Codable JWT support

--- Sources/APIMiddleware/TeamIDMiddleware.swift
+++ Sources/APIMiddleware/TeamIDMiddleware.swift
@@ -1,16 +1,23 @@
 import Vapor
+import JWT
 import Helpers
 
 public final class TeamIDMiddleware: Middleware {
     public init() {}
     
     public func respond(to request: Request, chainingTo next: Responder) throws -> Future<Response> {
-        let payload = try request.payload()
-        let teams: [Int]? = try payload.get("team_ids")
-        
+        let teams = try request.payload(as: TeamIDs.self).ids
+ 
         try request.set("skelpo_teams", to: teams)
         try request.set(teamMiddlewareKey, to: true)
         
         return try next.respond(to: request)
     }
+}
+
+fileprivate class TeamIDs: JWTPayload {
+    let ids: [Int]?
+    
+    // This method is empty because `JWTAuthenticationMiddleware` already varified the payload.
+    func verify() throws {}
 }
