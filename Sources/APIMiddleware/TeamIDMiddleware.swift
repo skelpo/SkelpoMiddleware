78893021f8af1d1939ab6b54d13b3c5c1b23b726
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-30 06:56:40 -0600

    Updated Request storage API usage in TeamIDMiddleware

--- Sources/APIMiddleware/TeamIDMiddleware.swift
+++ Sources/APIMiddleware/TeamIDMiddleware.swift
@@ -2,14 +2,14 @@ import Vapor
 import Helpers
 
 public final class TeamIDMiddleware: Middleware {
-    
     public init() {}
     
-    public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
+    public func respond(to request: Request, chainingTo next: Responder) throws -> Future<Response> {
         let payload = try request.payload()
         let teams: [Int]? = try payload.get("team_ids")
-        request.storage["skelpo_teams"] = teams
-        request.storage[teamMiddlewareKey] = true
+        
+        try request.set("skelpo_teams", to: teams)
+        try request.set(teamMiddlewareKey, to: true)
         
         return try next.respond(to: request)
     }
