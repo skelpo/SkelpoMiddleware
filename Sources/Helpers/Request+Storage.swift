f54ce3ec84a61ea85aa9886fa9e48f1d1aeaf015
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-30 06:55:06 -0600

    Made Request storage setter and getter public

--- Sources/Helpers/Request+Storage.swift
+++ Sources/Helpers/Request+Storage.swift
@@ -1,12 +1,12 @@
 import Vapor
 
 extension Request {
-    func get(_ key: String)throws -> Codable? {
+    public func get(_ key: String)throws -> Codable? {
         let storage = try self.privateContainer.make(Storage.self, for: Request.self)
         return storage.cache[key]
     }
     
-    func set(_ key: String, to value: Codable)throws {
+    public func set(_ key: String, to value: Codable)throws {
         var storage = try self.privateContainer.make(Storage.self, for: Request.self)
         storage.cache[key] = value
     }
