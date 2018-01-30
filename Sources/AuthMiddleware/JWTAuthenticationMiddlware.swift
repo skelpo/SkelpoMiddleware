6e4cfa4300f076333f44c755174e3f02189a5913
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-30 12:05:30 -0600

    Rewrote JWTAuthenticationMiddleware for Vapor 3

--- Sources/AuthMiddleware/JWTAuthenticationMiddlware.swift
+++ Sources/AuthMiddleware/JWTAuthenticationMiddlware.swift
@@ -1,56 +1,23 @@
 import Vapor
 import HTTP
-import JWTProvider
 import JWT
-import AuthProvider
+import Authentication
 import Helpers
 
-public final class JWTAuthenticationMiddleware: Middleware {
-    private(set) var signers: SignerMap
-    let url: String
-    let claims: [Claim]
-    let client: ClientFactoryProtocol
+public final class JWTAuthenticationMiddleware<Payload: JWTPayload>: Middleware {
+    let signerAlgorithm: JWTAlgorithm
     
-    public init(url: String, claims: [Claim] = []) {
-        self.url = url
-        self.claims = claims
-        
-        self.signers = SignerMap()
-        self.client = EngineClientFactory()
+    public init(algorithm: JWTAlgorithm) {
+        self.signerAlgorithm = algorithm
     }
     
-    public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
-        request.services.register(isSingleton: true) { (container) -> (Service) in
-            return Storage()
-        }
+    public func respond(to request: Request, chainingTo next: Responder) throws -> Future<Response> {        
+        let accessToken = try request.getJWT()
+        let signer = JWTSigner(algorithm: signerAlgorithm)
+        let jwt = try JWT<Payload>(from: accessToken, verifiedUsing: signer)
         
-        let jwt = try request.parseJWT()
-        let signer = try self.signer(for: jwt)
-        _ = try request.jwt(verifyUsing: signer, and: claims)
-        request.storage["skelpo-payload"] = jwt.payload
+        try request.set("skelpo-payload", to: jwt.payload)
         
         return try next.respond(to: request)
-    }
-    
-    private func signer(for jwt: JWT) throws -> Signer {
-        guard let kid = jwt.keyIdentifier else {
-            throw JWTProviderError.noVerifiedJWT
-        }
-        
-        if let signer = self.signers[kid] {
-            return signer
-        }
-        
-        guard let jwks = try self.client.get(url).json else {
-            throw JWTProviderError.noJWTSigner
-        }
-        
-        self.signers = try SignerMap(jwks: jwks)
-        
-        guard let signer = self.signers[kid] else {
-            throw JWTProviderError.noJWTSigner
-        }
-        
-        return signer
     }
 }
