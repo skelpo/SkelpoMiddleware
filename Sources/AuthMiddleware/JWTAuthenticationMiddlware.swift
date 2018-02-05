a38a26e8bd1c5400510e6b7f6c745a1392851a2b
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-05 15:46:46 -0600

    Store access token payload as JSON in JWTAuthenticationMiddleware

--- Sources/AuthMiddleware/JWTAuthenticationMiddlware.swift
+++ Sources/AuthMiddleware/JWTAuthenticationMiddlware.swift
@@ -3,6 +3,7 @@ import HTTP
 import JWT
 import Authentication
 import Helpers
+import Foundation
 
 public final class JWTAuthenticationMiddleware<Payload: JWTPayload>: Middleware {
     let signerAlgorithm: JWTAlgorithm
@@ -11,12 +12,12 @@ public final class JWTAuthenticationMiddleware<Payload: JWTPayload>: Middleware 
         self.signerAlgorithm = algorithm
     }
     
-    public func respond(to request: Request, chainingTo next: Responder) throws -> Future<Response> {        
+    public func respond(to request: Request, chainingTo next: Responder) throws -> Future<Response> {
         let accessToken = try request.getJWT()
         let signer = JWTSigner(algorithm: signerAlgorithm)
         let jwt = try JWT<Payload>(from: accessToken, verifiedUsing: signer)
         
-        try request.set("skelpo-payload", to: jwt.payload)
+        try request.set("skelpo-payload", to: JSONEncoder().encode(jwt.payload))
         
         return try next.respond(to: request)
     }
