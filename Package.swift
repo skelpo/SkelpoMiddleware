c67a33d85a45bd6aacf45775519ffe5106c9f472
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-28 10:46:10 -0500

    Updated JWTProvider version to 0.3.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
-        .package(url: "git@github.com:skelpo/JWTProvider.git", from: "0.2.0")
+        .package(url: "git@github.com:skelpo/JWTProvider.git", from: "0.3.0")
     ],
     targets: [
         .target(name: "SkelpoMiddleware", dependencies: ["Errors", "Helpers", "AuthMiddleware", "APIMiddleware", "JWTProvider"]),
