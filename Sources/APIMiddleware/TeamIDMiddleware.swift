79ed92a865a6011406e5b0bc68fb454d93f6db81
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-23 11:11:09 -0500

    Re-added Request.teams(_:) method

--- Sources/APIMiddleware/TeamIDMiddleware.swift
+++ Sources/APIMiddleware/TeamIDMiddleware.swift
@@ -1,8 +1,15 @@
 import VaporRequestStorage
 import JWTMiddleware
+import Foundation
 import Vapor
 
+// MARK: - Middleware
+
 public let teamMiddlewareKey = "team_id_middleware_registered"
+
+fileprivate class TeamIDs: Decodable {
+    let ids: [Int]?
+}
 
 public final class TeamIDMiddleware<PayloadType: Codable>: Middleware {
     public init() {}
@@ -17,6 +24,39 @@ public final class TeamIDMiddleware<PayloadType: Codable>: Middleware {
     }
 }
 
-fileprivate class TeamIDs: Decodable {
-    let ids: [Int]?
+// MARK: - Helpers
+
+extension Request {
+    @discardableResult
+    public func teams(_ teams: [Int]? = nil)throws -> [Int] {
+        let session = try self.session()
+        if let ids = teams {
+            try session.set("teams", to: ids)
+        }
+        
+        if let teams: [Int] = try session.get("teams", as: [Int]?.self) {
+            return teams
+        } else if let teams = try self.get("skelpo_teams", as: [Int].self) {
+            return teams
+        } else {
+            if let _ = try self.get(teamMiddlewareKey, as: Bool.self) {
+                return []
+            } else {
+                throw Abort(.internalServerError, reason: "TeamIDMiddleware has not been registered on the route you are attempting to get team IDs from")
+            }
+        }
+    }
+}
+
+extension Session {
+    public func set<Value: Codable>(_ key: String, to value: Value)throws {
+        self[key] = try String(data: JSONEncoder().encode(value), encoding: .utf8)
+    }
+    
+    public func get<Value: Codable>(_ key: String, as type: Value.Type = Value.self)throws -> Value {
+        guard let data = self[key]?.data(using: .utf8) else {
+            throw Abort(.internalServerError, reason: "Unable to convert `String` stored in session to `Data`")
+        }
+        return try JSONDecoder().decode(Value.self, from: data)
+    }
 }
