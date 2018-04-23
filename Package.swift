1351d29b3845200dc07ffb08b99ebc29736481f7
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-23 07:47:21 -0500

    Installed VaporRequestStorage package. Fixed JWTVapor package URL

--- Package.swift
+++ Package.swift
@@ -12,12 +12,13 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
-        .package(url: "https://github.com/skelpo/JWTVapor.git,", from: "0.3.0"),
+        .package(url: "https://github.com/skelpo/JWTVapor.git", from: "0.7.0"),
         .package(url: "https://github.com/skelpo/JWTMiddleware.git", from: "0.3.0"),
-        .package(url: "https://github.com/skelpo/APIErrorMiddleware.git", from: "0.1.0")
+        .package(url: "https://github.com/skelpo/APIErrorMiddleware.git", from: "0.1.0"),
+        .package(url: "https://github.com/skelpo/vapor-request-storage", from: "0.1.0")
     ],
     targets: [
-        .target(name: "SkelpoMiddleware", dependencies: ["Errors", "Helpers", "JWTMiddleware", "APIMiddleware", "JWTVapor", "APIErrorMiddleware"]),
+        .target(name: "SkelpoMiddleware", dependencies: ["Errors", "Helpers", "JWTMiddleware", "APIMiddleware", "JWTVapor", "APIErrorMiddleware", "VaporRequestStorage"]),
         .target(name: "APIMiddleware", dependencies: ["Errors", "Helpers", "Vapor"]),
         .target(name: "Helpers", dependencies: ["Errors", "Vapor", "JWT", "Authentication"]),
         .target(name: "Errors", dependencies: ["Vapor", "JWT"]),
