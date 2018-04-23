90e7a23e3a07c74d03c00b261554e01c005eef7a
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-23 07:28:50 -0500

    Deleted APIErrorMiddleware class

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
-        .package(url: "https://github.com/skelpo/JWTVapor.git", from: "0.3.0"),
+        .package(url: "https://github.com/skelpo/JWTVapor.git,", from: "0.3.0"),
         .package(url: "https://github.com/skelpo/JWTMiddleware.git", from: "0.3.0"),
         .package(url: "https://github.com/skelpo/APIErrorMiddleware.git", from: "0.1.0")
     ],
